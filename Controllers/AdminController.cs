5f1add12a62af88e67a7c73e06dd4ca8394497d5
HoangXuanLoc <hoanglocth@gmail.com>
2021-12-21 10:36:07 +0700

    Create AdminController and fix Layout

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using FPTAppDev.Models;
+using FPTAppDev.Utils;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FPTAppDev.Controllers
+{
+    [Authorize(Roles = Role.Admin)]
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext _context;
+        private ApplicationUserManager _userManager;
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public AdminController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+            _context = new ApplicationDbContext();
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Staff List
+        public ActionResult StaffList()
+        {
+            return View();
+        }
+        //Trainer List
+        public ActionResult TrainerList()
+        {
+            return View();
+        }
+
+    }
+}
