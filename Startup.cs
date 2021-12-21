7e51e28bbe03941ccd992f1895902159df919174
HoangXuanLoc <hoanglocth@gmail.com>
2021-12-21 08:44:57 +0700

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FPTAppDev.Startup))]
+namespace FPTAppDev
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
