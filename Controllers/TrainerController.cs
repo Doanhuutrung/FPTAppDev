f227b31d8d3f7ded77563069b6e2c1581f4b2f41
Hữu Trung Đoàn <huutrungdoan2202@gmail.com>
2021-12-24 13:17:15 +0700

    Create Trainer Controller

---
+++ Controllers/TrainerController.cs
@@ -0,0 +1,28 @@
+﻿using FPTAppDev.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FPTAppDev.Controllers
+{
+    public class TrainerController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TrainerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Trainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TrainerInfo()
+        {
+            var userId = User.Identity.GetUserId();
+            var trainerInDb = _context.TrainerDbset
+                .SingleOrDefault(t => t.TrainerId == userId);
+            return View(trainerInDb);
+        }
