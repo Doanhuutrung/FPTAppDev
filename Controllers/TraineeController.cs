dea4da1e802a717c527df628c414de4636a532ec
Hữu Trung Đoàn <huutrungdoan2202@gmail.com>
2021-12-24 13:57:53 +0700

    Create Trainee

---
+++ Controllers/TraineeController.cs
@@ -0,0 +1,23 @@
+﻿using FPTAppDev;
+using FPTAppDev.Models;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace FPTAppDev.Controllers
+{
+    public class TraineeController : Controller
+    {
+        private ApplicationDbContext _context;
+        public TraineeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Trainee
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
